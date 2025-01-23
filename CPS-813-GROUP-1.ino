a65720008a3b1c7ef607bfa2f852fad88d4c20d3
RZK27 <sekandarrahabar@gmail.com>
2025-01-23 02:22:36 -0500

    Updated movement

--- CPS-813-GROUP-1.ino
+++ CPS-813-GROUP-1.ino
@@ -11,15 +11,38 @@ void setup(){
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  move(0, 10);
+  // test movement
+  forward();
+  delay(3000);
+  brake();
+  delay(1000);
+  backward();
+  delay(3000);
+  brake();
+  delay(1000);
+  rotateLeft();
+  delay(1000);
+  brake();
+  delay(1000);
+  rotateRight();
+  delay(2000);
+  brake();
+  delay(1000);
+  rotateLeft();
+  delay(1000);
+  brake();
+  delay(1000);
 }
 
+//--MOVEMENT----
+
+// --add optional argument for time spent doing action (do not use delay)
 // moves robot according to x (rotation) and y (forward or backward) arguments
-// x and y are expected to be between -50 and 50 (inclusive)
+// x and y are expected to be between -10 and 10 (inclusive)
+// the speeds of move will end up between -100 and 100
 void move(int x, int y){
-    int left_speed = -x + y;
-    int right_speed = x + y;
+    int left_speed = x * x + y * y;
+    int right_speed = -x * x + y * y;
 
     if (left_speed > 0) {
         digitalWrite(left_direction_pin, HIGH);
@@ -37,3 +60,29 @@ void move(int x, int y){
         analogWrite(right_speed_pin, -right_speed);
     }
 }
+
+void brake(){
+  move(0, 0);
+}
+
+void forward(){
+  move(0, 7);
+}
+
+void backward(){
+  move(0, -7);
+}
+
+void rotateLeft(){
+  move(-7, 0);
+}
+
+void rotateRight(){
+  move(7, 0);
+}
+
+//--SENSORS----
+
+void readIR();
+
+void readUltrasonic();
