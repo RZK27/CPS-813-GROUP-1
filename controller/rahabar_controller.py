534762c061e03002eaa866be567405858a9884b3
RZK27 <sekandarrahabar@gmail.com>
2025-01-29 00:33:07 -0500

    added keyboard controls

--- controller/rahabar_controller.py
+++ controller/rahabar_controller.py
@@ -1,48 +1,50 @@
-from pynput import keyboard
 import requests
 import os
+import keyboard
 
 # Replace this with the Arduino's IP address and port
-arduino_url = "http://192.168.0.100"  # Example IP address
+arduino_url = "http://192.168.0.0"  # Example IP address
 
 def send_command_to_arduino(command):
     try:
         # Send a POST request to the Arduino with the command
         response = requests.post(arduino_url, command)
-        clear_and_print(response.text)
+        return(response.text)
     except Exception as e:
-        clear_and_print(f"Error sending command: {e}")
-
-def on_press(key):
-    try:
-        if key == keyboard.Key.up:
-            send_command_to_arduino("[F]")
-        elif key == keyboard.Key.down:
-            send_command_to_arduino("[B]")
-        elif key == keyboard.Key.left:
-            send_command_to_arduino("[L]")
-        elif key == keyboard.Key.right:
-            send_command_to_arduino("[R]")
-    except Exception as e:
-        print(f"Error: {e}")
+        return(f"Error sending command: {e}")
 
 def clear_and_print(message):
     # Clear the terminal
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('hi')
     # Print the message
     print(message)
+import keyboard
 
-# Set up the listener for key presses
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+# Declare the global speed variable
+speed = 0
 
-def main():
-    clear_and_print(requests.get(arduino_url).text)
-    print("Listening for arrow key presses...")
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
+def on_key_event(e):
+    data = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-if __name__ == "__main__":
-    print('hi')
-    #main()
+    if keyboard.is_pressed('s'):
+        speed = 8
+    else:
+        speed = 4
+
+    if keyboard.is_pressed('left'):
+        data[0] -= speed
+    if keyboard.is_pressed('right'):
+        data[0] += speed
+    if keyboard.is_pressed('up'):
+        data[1] += speed
+    if keyboard.is_pressed('down'):
+        data[1] -= speed
+
+    reponse_text = send_command_to_arduino(str(data))
+    clear_and_print(reponse_text)
+
+# Register key event listener
+keyboard.hook(on_key_event)
+
+# Block the program and listen for key events
+keyboard.wait()
